5f8ef10ca6399253d070cf144ab8445f98c424d6
Guchoo <ourg_oedesnel@hotmail.com>
2016-02-19 18:19:57 +0100

    Added a example project

--- WeatherApp/app/src/main/java/com/guroodesneltvedt/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/guroodesneltvedt/weatherapp/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        new WeatherActivity();
 
 /*        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
